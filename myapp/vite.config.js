725da0695c5bd8da613119e657ff0b63bca51b07
Ali Khan <128355297+alikhan09@users.noreply.github.com>
2025-03-22 01:39:21 +0500

    Fixed changes in script

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
- // base: "/Coffee-Hub/", // Must match your GitHub repo name
+  base: "/Coffee-Hub/", // Must match your GitHub repo name
   plugins: [react(), tailwindcss()],
 })
